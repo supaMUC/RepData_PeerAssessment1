3d8c7f44f3a252471a1d5c6ea0af0fd8395ff5a5
Matthias <matthias.stierle@gmail.com>
2014-07-09 20:31:09 +0200

    Deleted unnecessary R file

--- Code.R
+++
@@ -1,5 +0,0 @@
-data <- read.csv(unz("activity.zip", "activity.csv"), header=T, quote="\"", ## read data
-                 sep=",",colClasses=c("numeric","factor","numeric"))
-
-data$date <- as.Date(data$date)  ##Convert date column to Date type
-
